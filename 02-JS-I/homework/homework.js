0be71e1c9457bb4093221db6b508efb54d834db0
Daniel Erazo <erazoh95@hotmail.com>
2022-09-14 14:31:09 -0500

    Homework 1 completed

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -209,7 +209,7 @@ function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
-  var combinarNombre = (nombre + '' + apellido);
+  var combinarNombre = (nombre+" "+apellido);
   return combinarNombre;
 }
 
@@ -217,27 +217,31 @@ function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  
+  var ObtenerSaludo = ('Hola ' + nombre +'!');
+  return ObtenerSaludo;
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
   // Retornar el area de un rectángulo teniendo su altura y ancho
   // Tu código:
-  
+  var AreaRectangulo = (alto*ancho);
+  return AreaRectangulo;
 }
 
 
 function retornarPerimetro(lado){
   //Escibe una función a la cual reciba el valor del lado de un cuadrado y retorne su perímetro.
   //Escribe tu código aquí
-  
+  var Perimetro = (lado+lado+lado+lado);
+  return Perimetro;
 }
 
 
 function areaDelTriangulo(base, altura){
   //Desarrolle una función que calcule el área de un triángulo.
   //Escribe tu código aquí
-
+var AreaTriangulo = ((base*altura)/2);
+return AreaTriangulo;
 }
 
 
@@ -245,7 +249,8 @@ function deEuroAdolar(euro){
   //Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
   //como parámetro un número de euros y calcule el cambio en dólares.
   //Escribe tu código aquí
-  
+  var EurotoDolar = (euro*1.20);
+  return EurotoDolar;
 }
 
 
@@ -255,8 +260,19 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  
+
+if (letra.length > 1){
+  return "Dato incorrecto"
 }
+if (letra === 'a'|| letra === 'e'|| letra === 'i'|| letra === 'o'|| letra === 'u'){
+  return "Es vocal";
+}
+if (letra !== 'a'|| letra !== 'e'|| letra !== 'i'|| letra !== 'o'|| letra !== 'u'){
+  return "Dato incorrecto";
+}
+}
+
+
 
 
 
