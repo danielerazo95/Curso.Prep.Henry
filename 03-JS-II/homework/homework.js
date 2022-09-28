13f57d9ecfe235edeb2f2663014436dc4b247e32
Daniel Erazo <erazoh95@hotmail.com>
2022-09-28 15:30:08 -0500

    voy en esPrimo

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -135,6 +135,18 @@ function fizzBuzz(numero) {
   // Si "numero" es divisible entre 5, devuelve "buzz"
   // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
   // De lo contrario, devuelve el numero
+  if (numero % 3 == 0 && numero % 5 == 0){
+    return 'fizzbuzz';
+  } 
+  if (numero % 3 === 0) {
+    return 'fizz';
+  }
+  if (numero % 5 === 0) {
+    return 'buzz';
+  }
+  else {
+    return numero;
+  }
 }
 
 function operadoresLogicos(num1, num2, num3) {
@@ -144,6 +156,22 @@ function operadoresLogicos(num1, num2, num3) {
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
+  if (Math.sign(num1) === -1 || Math.sign(num2) === -1 || Math.sign(num3) === -1) {
+    return 'Hay negativos';
+  }
+  if (Math.sign(num1) === 0 || Math.sign(num2) === 0 || Math.sign(num3) === 0) {
+    return 'Error';
+  }
+  if(num1 > num2 && num1 > num3 && num1 >= 0){
+    return 'Número 1 es mayor y positivo';
+  }
+  if (num3 > num1 && num3 > num2) {
+    var numero3= num3 + 1;
+    return numero3;
+  }
+  else {
+    return false;
+  }
 }
 
 function esPrimo(numero) {
@@ -152,6 +180,16 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
+  if (numero <2){
+    return false;
+  }
+  for (var x=2; x<numero; x++){
+    if (numero % x === 0) {
+      return false;
+    }
+  }
+      return true;
+  
 }
 
 function esVerdadero(valor){
