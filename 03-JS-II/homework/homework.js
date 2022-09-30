978132c50dc2ed049881b188e28ecc95600f3e74
Daniel Erazo <erazoh95@hotmail.com>
2022-09-29 22:50:18 -0500

    Do doWhile

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -189,33 +189,45 @@ function esPrimo(numero) {
     }
   }
       return true;
-  
 }
 
 function esVerdadero(valor){
   //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
   //si su valor es true y “Soy falso” si su valor es false.
   //Escribe tu código aquí
-
+  if (valor === true) {
+    return 'Soy verdadero';
+  }
+  if (valor === false) {
+    return 'Soy falso';
+  }
 }
 
 function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí   
-  
+  tablaDelSeis = [0, 6, 12, 18, 24, 30, 36, 42, 48, 54, 60]
+  return tablaDelSeis;
 }
 
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
-  
+  var tiene3digitos = numero.toString();
+  if (tiene3digitos.length === 3){
+    return true;
+  }
+  else {
+    return false;
+  }
 }
 
 function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
   //Usar el bucle do ... while.
+  
 }
 
 
