f849bcc4cdb1d33f7429e902c9c4d7dabc96dd1d
Daniel Erazo <erazoh95@hotmail.com>
2022-10-05 14:00:41 -0500

    go to agregarNumeros

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -71,6 +71,8 @@ function arrayContiene(array, elemento) {
   // Comprueba si el elemento existe dentro de "array"
   // Devuelve "true" si está, o "false" si no está
   // Tu código:
+  var solucionContiene = array.includes(elemento);
+    return solucionContiene;
 }
 
 
